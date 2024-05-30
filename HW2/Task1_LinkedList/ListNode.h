3a7e26c777e8b6a48f78e45b0e1394480a4b4465
devjimy912 <devjimy912@gmail.com>
2024-05-30 13:56:01 +0900

    Update ListNode.h

--- HW2/Task1_LinkedList/ListNode.h
+++ HW2/Task1_LinkedList/ListNode.h
@@ -5,41 +5,10 @@
 
 template <typename T>
 class ListNode {
-private:
+public:
     T data;
     ListNode<T> *link;
-public:
-    ListNode();
-    void SetData(T data);
-    T GetData();
-    void SetLink(ListNode<T> *link);
-    ListNode<T>* GetLink();
+    ListNode(T item) : data(item), link(nullptr){};
 };
 
 #endif
-
-template <typename T>
-inline ListNode<T>::ListNode(){
-    data = T();
-    link = nullptr;
-}
-
-template <typename T>
-inline void ListNode<T>::SetData(T data){
-    this->data = data;
-}
-
-template <typename T>
-inline T ListNode<T>::GetData(){
-    return data;
-}
-
-template <typename T>
-inline void ListNode<T>::SetLink(ListNode<T> *link){
-    this->link = link;
-}
-
-template <typename T>
-inline ListNode<T> *ListNode<T>::GetLink(){
-    return link;
-}
