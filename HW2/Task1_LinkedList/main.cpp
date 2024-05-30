0c60d0e5e8aec42add753a8d8b2c7bba870910c4
devjimy912 <devjimy912@gmail.com>
2024-05-31 01:10:07 +0900

    2024.05.31_01:10

--- HW2/Task1_LinkedList/main.cpp
+++ HW2/Task1_LinkedList/main.cpp
@@ -8,6 +8,33 @@
 using namespace std;
 
 int main(){
+    //LinkedList();
+    LinkedList<int> list;
+    // ~LinkedList();
+
+    // void add(T item); // insert
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    // void removeLast();
+    list.removeLast();
+    // void removeFirst();
+    list.removeFirst();
+    // bool remove(T item);
+
+    // void removeAll();
+
+    // void invert();
+
+    // T get(T item) const; // retrieve
+
+    // T get(int index) const; // retrieve
+
+    // bool isEmpty() const;
+
+    // bool print() const;
+
     
     return 0;
 }
