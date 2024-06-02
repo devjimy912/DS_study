fd3ac2b05709b6ca5be39aa4de937848af1708e4
devjimy912 <devjimy912@gmail.com>
2024-06-03 06:46:50 +0900

    2024.06.03_06:46

--- HW2/Task2_MaxHeapAndMinHeap/MaxHeap.cpp
+++ HW2/Task2_MaxHeapAndMinHeap/MaxHeap.cpp
@@ -1,133 +1,134 @@
-#include "MaxHeap.h"
-#include <iostream>
+// #include "MaxHeap.h"
+// #include <iostream>
 
-template <typename T>
-MaxHeap::MaxHeap(const int &size){
-    this->list = new T[size];
-    this->maxSize = size;
-    this->last = -1;
-}
+// template <typename T>
+// MaxHeap<T>::MaxHeap(const int &size){
+//     this->list = new T[size];
+//     this->maxSize = size;
+//     this->last = -1;
+// }
 
-template <typename T>
-void MaxHeap::insert(const T& data){
-    //일단 값을 넣어
-    this->last++;
-    this->list[this->last] = data;
-    //그리고 값을 비교한다.
-    int curr = findParent(this->last);
-    int dummy = this->last;
-    while(curr){
-        if(this->list[curr] < this->last[dummy]){
-            int temp = this->list[curr];
-            this->list[curr] = this->last[dummy];
-            this->last[dummy] = temp;
-        }
-        dummy = curr;
-        curr = findParent(curr);
-    }
-    //마지막. 0번 인덱스와 비교
-    if(this->list[curr] < this->last[dummy]){
-        int temp = this->list[curr];
-        this->list[curr] = this->last[dummy];
-        this->last[dummy] = temp;
-    }
-}
+// template <typename T>
+// void MaxHeap<T>::insert(const T& data){
+//     //일단 값을 넣어
+//     this->last++;
+//     this->list[this->last] = data;
+//     //그리고 값을 비교한다.
+//     int curr = findParent(this->last);
+//     int dummy = this->last;
+//     while(curr){
+//         if(this->list[curr] < this->list[dummy]){
+//             int temp = this->list[curr];
+//             this->list[curr] = this->list[dummy];
+//             this->list[dummy] = temp;
+//         }
+//         dummy = curr;
+//         curr = findParent(curr);
+//     }
+//     //마지막. 0번 인덱스와 비교
+//     if(this->list[curr] < this->list[dummy]){
+//         int temp = this->list[curr];
+//         this->list[curr] = this->list[dummy];
+//         this->list[dummy] = temp;
+//     }
+// }
 
-int findParent(const int& index){
-    if(index%2 == 1){
-        return (index-1)/2;
-    }else{
-        return (index-2)/2;
-    }
-}
+// template <typename T>
+// int MaxHeap<T>::findParent(const int& index){
+//     if(index%2 == 1){
+//         return (index-1)/2;
+//     }else{
+//         return (index-2)/2;
+//     }
+// }
 
-template <typename T>
-T MaxHeap::extract(){// same as delete
-    //만약 비었다면 빈 값보내기
-    if(isEmpty()){
-        return T();
-    }
-    //일단 값 저장
-    T temp = this->list[0];
-    //마지막 값을 최상위로 보냄
-    this->list[0] = this->list[this->last];
-    this->last--;
-    //이제 리스트 전체를 스왚으로 헤짚으면 됨.
-    int num = 0;
-    while((num+1)*2 <= this->last){
-        swap(this->list[num], this->list[num*2+1]);
-        swap(this->list[num], this->list[num*2+2]);
-        num++;
-    }
+// template <typename T>
+// T MaxHeap<T>::extract(){// same as delete
+//     //만약 비었다면 빈 값보내기
+//     if(isEmpty()){
+//         return T();
+//     }
+//     //일단 값 저장
+//     T temp = this->list[0];
+//     //마지막 값을 최상위로 보냄
+//     this->list[0] = this->list[this->last];
+//     this->last--;
+//     //이제 리스트 전체를 스왚으로 헤짚으면 됨.
+//     int num = 0;
+//     while((num+1)*2 <= this->last){
+//         swap(this->list[num], this->list[num*2+1]);
+//         swap(this->list[num], this->list[num*2+2]);
+//         num++;
+//     }
 
-    if(num*2+1 == this->last){
-        swap(this->list[num*2+1], this->list[this->last]);
-    }
+//     if(num*2+1 == this->last){
+//         swap(this->list[num*2+1], this->list[this->last]);
+//     }
 
-    return temp;
+//     return temp;
 
-    /* 만들고 나서 swap메소드가 있는 걸 봤네요.
-    저거 사용하려면 다른 아래 방식이랑은 다른 로직을 써야해서 새로 만드는데
-    만든 거 지우기 아쉬워 남깁니다.
-    //값이 있다면 최상위 값 보내기
-    //일단 값 저장
-    T temp = this->list[0];
-    //이제 비교해야됨
-    int num = 0;
-    int left, right;
-    //비교조건1 : 자식이 둘인가
-    while((num+1)*2 <= this->last){ 
-        left = num*2+1;
-        right = num*2+2;
-        if(left < right){ //오른쪽이 더 큰거
-            this->list[num] = this->list[right];
-            num = right;
-        }else{ //왼쪽이 더 큰거
-            this->list[num] = this->list[left];
-            num = left;
-        }
-    }
-    //자식이 하나면 왼쪽거 올리고 없으면 끝.
-    //위에서는 자식이 하나 또는 안 남는 지점에서 멈춤.
-    //인덱스 없는거 건들면 안되니까 if로 한번 걸렀어야 했네
-    this->list[num] = this->list[this->last];
-    //마지막 단계
-    this->last--;
-    */
-}
+//     /* 만들고 나서 swap메소드가 있는 걸 봤네요.
+//     저거 사용하려면 다른 아래 방식이랑은 다른 로직을 써야해서 새로 만드는데
+//     만든 거 지우기 아쉬워 남깁니다.
+//     //값이 있다면 최상위 값 보내기
+//     //일단 값 저장
+//     T temp = this->list[0];
+//     //이제 비교해야됨
+//     int num = 0;
+//     int left, right;
+//     //비교조건1 : 자식이 둘인가
+//     while((num+1)*2 <= this->last){ 
+//         left = num*2+1;
+//         right = num*2+2;
+//         if(left < right){ //오른쪽이 더 큰거
+//             this->list[num] = this->list[right];
+//             num = right;
+//         }else{ //왼쪽이 더 큰거
+//             this->list[num] = this->list[left];
+//             num = left;
+//         }
+//     }
+//     //자식이 하나면 왼쪽거 올리고 없으면 끝.
+//     //위에서는 자식이 하나 또는 안 남는 지점에서 멈춤.
+//     //인덱스 없는거 건들면 안되니까 if로 한번 걸렀어야 했네
+//     this->list[num] = this->list[this->last];
+//     //마지막 단계
+//     this->last--;
+//     */
+// }
 
-template <typename T>
-void MaxHeap::swap(T& dataA, T& dataB){
-    if(dataA < dataB){
-        T dummy = dataA;
-        dataA = dataB;
-        dataB = dummy;
-    }
-}
+// template <typename T>
+// void MaxHeap<T>::swap(T& dataA, T& dataB){
+//     if(dataA < dataB){
+//         T dummy = dataA;
+//         dataA = dataB;
+//         dataB = dummy;
+//     }
+// }
 
-template <typename T>
-T MaxHeap::peek() const {// get the root item but not delete
-    if(isEmpty()){
-        return T();
-    }else{
-        return this->list[0];
-    }
-}
+// template <typename T>
+// T MaxHeap<T>::peek() const {// get the root item but not delete
+//     if(isEmpty()){
+//         return T();
+//     }else{
+//         return this->list[0];
+//     }
+// }
 
-template <typename T>
-bool MaxHeap::isEmpty() const {
-    if(this->last == -1){
-        return true;
-    }else{
-        return false;
-    }
-}
+// template <typename T>
+// bool MaxHeap<T>::isEmpty() const {
+//     if(this->last == -1){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
 
-template <typename T>
-bool MaxHeap::isFull() const {
-    if(this->last +1 == this->maxSize){
-        return true;
-    }else{
-        return false;
-    }
-}
+// template <typename T>
+// bool MaxHeap<T>::isFull() const {
+//     if(this->last +1 == this->maxSize){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
--- HW2/Task2_MaxHeapAndMinHeap/main.cpp
+++ HW2/Task2_MaxHeapAndMinHeap/main.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 
 int main(){
-    
+    cout<<"Program Start"<<endl;
+    //생성 어케 하지..
+    int num = 15;
+    MaxHeap<int> maxheap(num);
+    cout<<"Generate MaxHeap"<<endl;
+    maxheap.insert(1);
+    cout<<"Insert 1"<<endl;
+    maxheap.insert(2);
+    cout<<"Insert 2"<<endl;
+    maxheap.insert(3);
+    cout<<"Insert 3"<<endl;
+    cout<<"Max Heap is "<<maxheap.extract()<<endl;
+    cout<<"-> Pop"<<endl;
+    cout<<"Max Heap is "<<maxheap.peek()<<endl;
+    cout<<"-> Just veiw"<<endl;
 
 
     return 0;
