c633537292e754a8c64052788fa7252c68a019da
devjimy912 <devjimy912@gmail.com>
2024-06-03 21:59:07 +0900

    2024.06.03_21:59

--- HW2/Task3_BinarySearchTree/BinarySearchTree.h
+++ HW2/Task3_BinarySearchTree/BinarySearchTree.h
@@ -3,12 +3,15 @@
 
 #include "BinarySearchTreeInterface.h"
 
+using namespace std;
+
 template <typename T>
 class Node {
     // data fields
+public:
     T data;
-    Node *left_child, *right_child;
-    Node(data){
+    Node<T> *left_child, *right_child;
+    Node<T>(T data){
         this->data = data;
         left_child = nullptr;
         right_child = nullptr;
@@ -16,44 +19,41 @@ class Node {
 };
 
 template <typename T>
-class BinarySearchTree : public BinarySearchTreeInterface {
+class BinarySearchTree : public BinarySearchTreeInterface<T> {
 private:
-    tree_node* middle;
+    Node<T>* middle;
 
 public:
     // 요소를 트리에 삽입
-    virtual void insert(const T& key) = 0;
+    virtual void insert(const T& key);
 
     // 요소를 트리에서 삭제
-    virtual void remove(const T& key) = 0;
+    virtual void remove(const T& key);
 
     // 요소가 트리에 존재하는지 확인
-    virtual bool contains(const T& key) const = 0; // search
+    virtual bool contains(const T& key) const; // search
 
     // 트리에서 최소값 반환
-    virtual T findMin() const = 0;
+    virtual T findMin() const;
 
     // 트리에서 최대값 반환
-    virtual T findMax() const = 0;
+    virtual T findMax() const;
 
     // 트리가 비어있는지 확인
-    virtual bool isEmpty() const = 0;
+    virtual bool isEmpty() const;
 
     // 트리를 비움
-    virtual void clear() = 0;
-
-    // 가상 소멸자
-    virtual ~BinarySearchTreeInterface() {}
+    virtual void clear();
 };
 
 template <typename T>
 // 요소를 트리에 삽입
 void BinarySearchTree<T>::insert(const T& key){
-    Node node = new Node(key);
+    Node<T>* node = new Node<T>(key);
     if(middle == nullptr){
         middle = node;
     }else{
-        Node* temp = middle;
+        Node<T>* temp = middle;
         while(1){
             if(temp->data > node->data){
                 if(temp->left_child == nullptr){
@@ -77,9 +77,77 @@ void BinarySearchTree<T>::insert(const T& key){
 template <typename T>
 // 요소를 트리에서 삭제
 void BinarySearchTree<T>::remove(const T& key){
+    if(contains(key)){
+        cout<<"Doesn't exist "<<key<<endl;
+        return;
+    }
     //찾고 없애기
-    
-    //공백 메우기
+    Node<T>* temp = middle;
+    Node<T>* prev;
+    int a;
+    while(1){
+        if(temp->data > key){
+            prev=temp;
+            temp = temp->left_child;
+            a=1;//right
+        }else if(temp->data < key){
+            prev=temp;
+            temp = temp->right_child;
+            a=0;//left
+        }else{//찾음. 공백 메우기
+            if(temp->right_child == nullptr && temp->left_child == nullptr){
+                delete temp;
+                if(a){
+                    prev->right_child = nullptr;
+                }else{
+                    prev->left_child = nullptr;
+                }
+            }else if(temp->right_child != nullptr && temp->left_child == nullptr){
+                if(a){
+                    prev->right_child = temp->right_child;
+                }else{
+                    prev->left_child = temp->right_child;
+                }
+                delete temp;
+            }else if(temp->right_child == nullptr && temp->left_child != nullptr){
+                if(a){
+                    prev->right_child = temp->left_child;
+                }else{
+                    prev->left_child = temp->left_child;
+                }
+                delete temp;
+            }else{//둘다있는거
+                Node<T>* change = temp->left_child;
+                while(change->right_child != nullptr){
+                    prev = change;
+                    change = change->right_child;
+                }
+                temp->data = change->data;
+                if(change->right_child == nullptr && change->left_child == nullptr){
+                    delete change;
+                    if(a){
+                        prev->right_child = nullptr;
+                    }else{
+                        prev->left_child = nullptr;
+                    }
+                }else if(change->right_child != nullptr && change->left_child == nullptr){
+                    if(a){
+                        prev->right_child = change->right_child;
+                    }else{
+                        prev->left_child = change->right_child;
+                    }
+                    delete change;
+                }else if(change->right_child == nullptr && change->left_child != nullptr){
+                    if(a){
+                        prev->right_child = change->left_child;
+                    }else{
+                        prev->left_child = change->left_child;
+                    }
+                    delete change;
+                }
+            }
+        }
+    }
 }
 
 template <typename T>
@@ -87,14 +155,33 @@ template <typename T>
 bool BinarySearchTree<T>::contains(const T& key) const { // search
     //찾으면 트루
     //없으면 폴스
-
+    Node<T>* temp = middle;
+    while(1){
+        if(temp->data > key){
+            if(temp->left_child == nullptr){
+                return false;
+            }else{
+                temp = temp->left_child;
+            }
+        }else if(temp->data < key){
+            if(temp->right_child == nullptr){
+                return false;
+                break;
+            }else{
+                temp = temp->right_child;
+            }
+        }else{
+            return true;
+        }
+    }
+    
 }
 
 template <typename T>
 // 트리에서 최소값 반환
 T BinarySearchTree<T>::findMin() const {
     //널포인터 나올 때까지 왼쪽으로
-    Node* temp = middle;
+    Node<T>* temp = middle;
     while(temp->left_child != nullptr){
         temp = temp->left_child;
     }
@@ -105,7 +192,7 @@ template <typename T>
 // 트리에서 최대값 반환
 T BinarySearchTree<T>::findMax() const {
     //널포인터 나올 때까지 오른쪽으로
-    Node* temp = middle;
+    Node<T>* temp = middle;
     while(temp->right_child != nullptr){
         temp = temp->right_child;
     }
@@ -128,6 +215,24 @@ template <typename T>
 void BinarySearchTree<T>::clear() {
     //하나씩 지워야지
     //널이면 돌아가고 있으면 지우고
+    Node<T>* temp = middle;
+    while(1){
+        if(middle->right_child == nullptr && middle->left_child == nullptr){
+            break;
+        }
+        if(temp->right_child == nullptr && temp->left_child == nullptr){
+            delete temp;
+            temp = middle;
+        }else{
+            if(temp->right_child != nullptr){
+                temp = temp->right_child;
+            }else{
+                temp = temp->left_child;
+            }
+        }
+    }
+    delete middle;
+    middle = nullptr;
 }
 
 #endif // BINARY_SEARCH_TREE_INTERFACE_H
--- HW2/Task3_BinarySearchTree/main.cpp
+++ HW2/Task3_BinarySearchTree/main.cpp
@@ -4,8 +4,42 @@
 using namespace std;
 
 int main(){
+    BinarySearchTree<int> tree;
 
+    tree.insert(15);
+    tree.insert(13);
+    tree.insert(6);
+    tree.insert(5);
+    tree.insert(9);
+    tree.insert(17);
+    tree.insert(16);
+    tree.insert(30);
 
-    
+    if(tree.contains(17)){
+        cout<<"tree has 17"<<endl;
+    }else{
+        cout<<"not exist"<<endl;
+    }
+    tree.remove(17);
+    if(tree.contains(17)){
+        cout<<"tree has 17"<<endl;
+    }else{
+        cout<<"not exist"<<endl;
+    }
+    cout<<"Max: "<<tree.findMax()<<endl;
+    cout<<"Min: "<<tree.findMin()<<endl;
+
+    if(tree.isEmpty()){
+        cout<<"Empty"<<endl;
+    }else{
+        cout<<"Something exist"<<endl;
+    }
+    tree.clear();
+    if(tree.isEmpty()){
+        cout<<"Empty"<<endl;
+    }else{
+        cout<<"Something exist"<<endl;
+    }
+
     return 0;
 }
