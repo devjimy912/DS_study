1b27cb73d6fdeb29d1aaff2d07ae3dcc814cd8d2
devjimy912 <devjimy912@gmail.com>
2024-06-19 17:29:30 +0900

    2024.06.19_17:29

--- HW4/Task1_AVLTree/main.cpp
+++ HW4/Task1_AVLTree/main.cpp
@@ -12,50 +12,56 @@ int main() {
     avlTree.Insert(7, "Node 7");
     avlTree.Insert(5, "Node 5");
     std::cout << "Tree structure after LL case:\n";
-    //왼쪽 자식의 왼쪽 추가
     avlTree.PrintTree();
     std::cout << std::endl;
 
-    std::cout << "Inserting LR case: 10, 7, 5, 1, 3\n";
-    avlTree.Insert(13, "Node 1");
-    avlTree.Insert(16, "Node 3");
-    std::cout << "Tree structure after RR case:\n";
-    //왼쪽 자식의 오른쪽 추가
-    avlTree.PrintTree();
-    std::cout << std::endl;
-
-    std::cout << "Inserting RR case: 10, 5, 15, 12, 20\n";
-    avlTree.Insert(20, "Node 20");
+    std::cout << "Inserting LR case: 10, 7, 5, 3, 1\n";
+    avlTree.Insert(3, "Node 3");
+    avlTree.Insert(1, "Node 1");
     std::cout << "Tree structure after LR case:\n";
-    //오른쪽 자식의 오른쪽 추가
     avlTree.PrintTree();
     std::cout << std::endl;
 
-    std::cout << "Inserting RL case: 10, 5, 15, 12, 17\n";
-    avlTree.Insert(17, "Node 17");
-    std::cout << "Tree structure after RL case:\n";
-    //오른쪽 자식의 왼쪽 추가
+    std::cout << "Deleting R0 case: 10\n";
+    avlTree.Delete(10);
+    std::cout << "Tree structure after R0 case:\n";
     avlTree.PrintTree();
     std::cout << std::endl;
 
-    // Deletion tests
-    std::cout << "Deleting L0 case: 5\n";
-    avlTree.Delete(5);
-    std::cout << "Tree structure after L0 case:\n";
-    avlTree.PrintTree();
-    std::cout << std::endl;
-
-    std::cout << "Deleting L1 case: 12\n";
-    avlTree.Delete(12);
+    std::cout << "Deleting L1 case: 1\n";
+    avlTree.Delete(1);
     std::cout << "Tree structure after L1 case:\n";
     avlTree.PrintTree();
     std::cout << std::endl;
 
-    std::cout << "Deleting L-1 case: 15\n";
-    avlTree.Delete(15);
-    std::cout << "Tree structure after L-1 case:\n";
+    std::cout << "Inserting RR case: 3, 5, 7, 9, 11\n";
+    avlTree.Insert(9, "Node 9");
+    avlTree.Insert(11, "Node 11");
+    std::cout << "Tree structure after RR case:\n";
     avlTree.PrintTree();
     std::cout << std::endl;
+
+    std::cout << "Deleting L0 case: 3\n";
+    avlTree.Delete(3);
+    std::cout << "Tree structure after L0 case:\n";
+    avlTree.PrintTree();
+    std::cout << std::endl;
+
+    std::cout << "Deleting R-1 case: 11\n";
+    avlTree.Delete(11);
+    std::cout << "Tree structure after R-1 case:\n";
+    avlTree.PrintTree();
+    std::cout << std::endl;
+
+    std::cout << "Inserting RL case: 5, 7, 9, 11, 13\n";
+    avlTree.Insert(13, "Node 13");
+    avlTree.Insert(11, "Node 11");
+    std::cout << "Tree structure after RL case:\n";
+    avlTree.PrintTree();
+    std::cout << std::endl;
+
+    //Remaining : L-1, R1
+    
 
     return 0;
 }
